facee86661632eae15df4c1d9020c3c1515d646e
Shabuktagin Photon Khan <khan.photon@gmail.com>
2018-10-27 09:43:53 +0600

    Added Authentication and Signout

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,5 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout
 
-# Create your views here.
-def signup_view(request):
-    return render(request, 'accounts/signup.html', {})
+def signup_view(request, *args, **kwargs):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:article-list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/signup.html', {'form': form})
+
+def login_view(request, *args, **kwargs):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('articles:article-list')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {"form": form})
+
+def logout_view(request, *args, **kwargs):
+    if request.method == "POST":
+        logout(request)
+        return redirect('articles:article-list')
