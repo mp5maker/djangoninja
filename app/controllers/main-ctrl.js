ba87bbf0b62a6842087e6cc4de087456bde3a06b
Shabuktagin Photon Khan <khan.photon@gmail.com>
2018-11-27 22:39:40 +0600

    Remove Service Worker

--- app/controllers/main-ctrl.js
+++ app/controllers/main-ctrl.js
@@ -7,36 +7,6 @@
     function Controller($scope, graphQLFactory) {
         $scope.heading = "Checking";
         
-        if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('/static/serviceworker.js', {scope: '/static/'})
-            .then((registration) => {
-                var serviceWorker;
-                if (registration.installing) {
-                    serviceWorker = registration.installing;
-                } else if (registration.waiting) {
-                    serviceWorker = registration.waiting;
-                } else if (registration.active) {
-                    serviceWorker = registration.active;
-                }
-
-                if (serviceWorker) {
-                    console.log('Service Worker Phase: ', serviceWorker.state);
-                    serviceWorker.addEventListener('statechange', (event) => {
-                        console.log('Service Worker Phase: ' + event.target.state);
-                    })
-                }
-            }).catch((error) => {
-                console.log("Service Worker registration failed: ", error);
-            })
-        } else {
-            console.log('Browser do not supports service worker');
-        }
-
-        // navigator.serviceWorker.ready
-        // .then((serviceWorkerRegistration) => {
-        //     return serviceWorkerRegistration.sync.register('foo');
-        // })
-
         var query = `
             query {
                 allArticles {
